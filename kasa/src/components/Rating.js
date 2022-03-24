63c7085cee938214a4ce3cb291b712903b51a2a3
Thomas Castro <contact.thomas.Castro@gmail.com>
2022-03-24 13:45:57 +0100

    Rating component display stars

--- kasa/src/components/Rating.js
+++ kasa/src/components/Rating.js
@@ -1,9 +1,25 @@
+import { RiStarSFill } from 'react-icons/ri';
+import "../styles/Rating.css"
+
 function Rating(props) {
     const rating = props.rating
+    const stars = ['', '', '', '', '']
+
+    stars.forEach((element, index) => { 
+        if (index < rating) {
+            stars[index] = 'filled'
+        } else {
+            stars[index] = 'empty'
+        }   
+    });
+
+    console.log(stars);
 
     return (
         <div>
-            <span>Noté {rating} / 5</span>
+            {stars.map( (star, index) => (
+                <RiStarSFill key={index} className={star + ' rating-star'} />
+            ))}
         </div>
     )
 }
